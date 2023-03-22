119802df0d40fb6969c5df399a4855b01aabd114
Mar <s17120246@alumnos.itsur.edu.mx>
2023-03-22 14:55:47 -0600

    final commit

--- app/src/main/java/com/example/task_admin/MainActivity.kt
+++ app/src/main/java/com/example/task_admin/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.task_admin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.task_admin.ui.theme.Task_AdminTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +25,43 @@ class MainActivity : ComponentActivity() {
         setContent {
             Task_AdminTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            tskAdmn()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun tskcomplete(modifier: Modifier=Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null)
+        Text(
+            stringResource(R.string.title),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            stringResource(R.string.congrats),
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun tskAdmn() {
     Task_AdminTheme {
-        Greeting("Android")
+        tskcomplete(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+        )
     }
 }
